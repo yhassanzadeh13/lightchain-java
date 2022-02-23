d8f2946d784f6d5fd8cd3fe9b46f99207fd31bab
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2022-02-23 06:23:01 -0800

    adds documentation for the identifier

--- src/main/java/model/lightchain/Identifier.java
+++ src/main/java/model/lightchain/Identifier.java
@@ -1,11 +1,17 @@
 package model.lightchain;
 
-
+/**
+ * Represents a 32-byte unique identifier for an entity. Normally is computed as the hash value of the entity.
+ */
 public class Identifier {
   public static final int Size = 32;
   private final byte[] value;
 
   public Identifier(byte[] value) {
     this.value = value;
+  }
+
+  public byte[] getBytes() {
+    return this.value;
   }
 }
