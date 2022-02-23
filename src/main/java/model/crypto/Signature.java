3fa5f3f52f65a662a9a19d17d7559ddefa7cac71
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2022-02-23 06:08:46 -0800

    adds java doc for signature.

--- src/main/java/model/crypto/Signature.java
+++ src/main/java/model/crypto/Signature.java
@@ -1,9 +1,15 @@
 package model.crypto;
 
+/**
+ * Represents abstract data type for the cryptographic digital signature used in LightChain.
+ */
 public abstract class Signature {
+  /**
+   * The signature value in bytes.
+   */
   private final byte[] bytes;
 
-  protected Signature(byte[] bytes) {
+  public Signature(byte[] bytes) {
     this.bytes = bytes;
   }
 }
