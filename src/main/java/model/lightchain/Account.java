9bbee80a43c9e963ff990b0eb8ecc7d3303f0d6a
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2022-02-23 06:40:00 -0800

    adds documentation for account

--- src/main/java/model/lightchain/Account.java
+++ src/main/java/model/lightchain/Account.java
@@ -1,17 +1,42 @@
 package model.lightchain;
 
-import model.crypto.Hash;
 import model.crypto.PublicKey;
 
+/**
+ * Represents a LightChain account which is the constituent of the SnapShot.
+ */
 public class Account {
+  /**
+   * Unique identifier of the account.
+   */
   private final Identifier identifier;
-  private final PublicKey publicKey;
-  private double balance;
-  private Hash lastBlockId;
 
-  public Account(Identifier identifier, PublicKey publicKey) {
+  /**
+   * Public key corresponding to this account.
+   */
+  private final PublicKey publicKey;
+
+  /**
+   * Account balance in LightChain tokens.
+   */
+  private final double balance;
+
+  /**
+   * The identifier of last finalized block that changed (balance of) this account.
+   */
+  private Identifier lastBlockId;
+
+  /**
+   * Constructor of an Account.
+   *
+   * @param identifier unique identifier of the account.
+   * @param publicKey public key of the account owner.
+   * @param lastBlockId identifier of the last block id that changed this account (or genesis id at bootstrap time).
+   */
+  public Account(Identifier identifier, PublicKey publicKey, Identifier lastBlockId) {
     this.identifier = identifier;
     this.publicKey = publicKey;
+    this.lastBlockId = lastBlockId;
     this.balance = 0;
   }
 }
