ba5b452ad9156b9184782c0be557a014638ed6a3
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2022-03-02 05:59:13 -0800

    adds validated transaction

---
+++ src/main/java/model/lightchain/ValidatedTransaction.java
@@ -0,0 +1,37 @@
+package model.lightchain;
+
+import model.crypto.Signature;
+
+/**
+ * A ValidatedTransaction is a wrapper around a Transaction that carries a proof of assigned validators that attests
+ * the transaction passed local validation of validators.
+ */
+public class ValidatedTransaction extends Transaction{
+  /**
+   * Represents the signatures of assigned validators to this transaction.
+   */
+  private final Signature[] certificates;
+
+  /**
+   * Constructor of the transaction.
+   * 
+   *  @param referenceBlockId identifier of a finalized block that this transaction refers to its snapshot.
+   * @param sender           identifier of the sender of this transaction.
+   * @param receiver         identifier of the receiver of this transaction.
+   * @param amount           amount of LightChain tokens that this transaction transfers from sender to receiver.
+   * @param certificates     signature of assigned validators to this transaction.
+   */
+  public ValidatedTransaction(Identifier referenceBlockId,
+                              Identifier sender,
+                              Identifier receiver,
+                              double amount,
+                              Signature[] certificates) {
+
+    super(referenceBlockId, sender, receiver, amount);
+    this.certificates = certificates;
+  }
+
+  public Signature[] getCertificates() {
+    return certificates;
+  }
+}
