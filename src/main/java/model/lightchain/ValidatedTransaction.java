294a8cf1d4bfcd772c4ad65ef7b299da8484cd78
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-03-02 07:02:39 -0800

    [Protocol] Adds interface for block validation (#7)
    * adds validated transaction
    
    * adds block
    
    * adds parameters
    
    * adds javadoc for parameter class
    
    * adds validator and stake
    
    * adds comment for validators
    
    * adds test scenarios
    
    * fixes lint
    
    * fixes spotbugs
    
    * fixes spotbugs
    
    * fixes spotbugs

---
+++ src/main/java/model/lightchain/ValidatedTransaction.java
@@ -0,0 +1,43 @@
+package model.lightchain;
+
+import model.codec.EntityType;
+import model.crypto.Signature;
+
+/**
+ * A ValidatedTransaction is a wrapper around a Transaction that carries a proof of assigned validators that attests
+ * the transaction passed local validation of validators.
+ */
+public class ValidatedTransaction extends Transaction {
+  /**
+   * Represents the signatures of assigned validators to this transaction.
+   */
+  private final Signature[] certificates;
+
+  /**
+   * Constructor of the transaction.
+   *
+   * @param referenceBlockId identifier of a finalized block that this transaction refers to its snapshot.
+   * @param sender           identifier of the sender of this transaction.
+   * @param receiver         identifier of the receiver of this transaction.
+   * @param amount           amount of LightChain tokens that this transaction transfers from sender to receiver.
+   * @param certificates     signature of assigned validators to this transaction.
+   */
+  public ValidatedTransaction(Identifier referenceBlockId,
+                              Identifier sender,
+                              Identifier receiver,
+                              double amount,
+                              Signature[] certificates) {
+
+    super(referenceBlockId, sender, receiver, amount);
+    this.certificates = certificates.clone();
+  }
+
+  public Signature[] getCertificates() {
+    return certificates.clone();
+  }
+
+  @Override
+  public String type() {
+    return EntityType.TYPE_VALIDATED_TRANSACTION;
+  }
+}
