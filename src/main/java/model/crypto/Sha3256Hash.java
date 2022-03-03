aff1cbd79d168bc0ec07871cc0410bb48278593d
Ozan Nacitarhan <onacitarhan17@ku.edu.tr>
2022-03-03 14:19:49 +0300

    adds sanity checks for Sha3256Hash

--- src/main/java/model/crypto/Sha3256Hash.java
+++ src/main/java/model/crypto/Sha3256Hash.java
@@ -11,11 +11,17 @@ public class Sha3256Hash extends Hash {
 
   public Sha3256Hash(byte[] hashValue) {
     super(hashValue);
+    if (hashValue.length != 32) {
+      throw new IllegalArgumentException("Hash value must be 32 bytes long.");
+    }
     this.bytes = hashValue.clone();
   }
 
   public Sha3256Hash(Identifier identifier) {
     super(identifier);
+    if (identifier.getBytes().length != 32) {
+      throw new IllegalArgumentException("Identifier must be 32 bytes long.");
+    }
     this.bytes = identifier.getBytes();
   }
 
