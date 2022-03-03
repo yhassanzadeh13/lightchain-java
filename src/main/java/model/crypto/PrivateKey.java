c5c0427806bafbd8b6bc836ea4103e433e95544f
Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>
2022-03-03 06:57:13 -0800

    adds keygen and private key (#8)

---
+++ src/main/java/model/crypto/PrivateKey.java
@@ -0,0 +1,22 @@
+package model.crypto;
+
+import model.Entity;
+
+/**
+ * Represents abstract data type for the cryptographic private key used in LightChain.
+ */
+public abstract class PrivateKey {
+  private final byte[] bytes;
+
+  public PrivateKey(byte[] bytes) {
+    this.bytes = bytes.clone();
+  }
+
+  /**
+   * Signs the given entity using private key.
+   *
+   * @param e entity to sign.
+   * @return a signature over entity e using private key.
+   */
+  public abstract Signature signEntity(Entity e);
+}
