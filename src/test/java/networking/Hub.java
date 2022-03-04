0447016e891e0997eb0f1f8e89a34516b5b0a504
akucukoduk16 <akucukoduk16@ku.edu.tr>
2022-03-04 13:11:09 +0300

    add my name

--- src/test/java/networking/Hub.java
+++ src/test/java/networking/Hub.java
@@ -7,6 +7,7 @@ import model.lightchain.Identifier;
 import network.Network;
 
 public class Hub {
+  //abdullah
   private ConcurrentHashMap<Identifier, Network> networks;
   private ConcurrentHashMap<Identifier, Entity> entities;
 }
