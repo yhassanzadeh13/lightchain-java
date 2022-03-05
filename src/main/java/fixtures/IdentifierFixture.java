85b01684bd8f0ae8bf69226a94141047f745d4c4
saarantras <isahin16@ku.edu.tr>
2022-03-06 00:41:27 +0300

    more spotbug fix

--- src/main/java/fixtures/IdentifierFixture.java
+++
@@ -1,8 +0,0 @@
-package fixtures;
-
-public class IdentifierFixture {
-  public static model.lightchain.Identifier NewIdentifier() {
-    byte[] bytes = Bytes.ByteArrayFixture(model.lightchain.Identifier.Size);
-    return new model.lightchain.Identifier(bytes);
-  }
-}
