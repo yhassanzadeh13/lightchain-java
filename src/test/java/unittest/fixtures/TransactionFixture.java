b961e81bdf85098b26cac1c2ae15be4ce8b40ed5
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2022-03-06 06:46:40 -0800

    cleans up transaction fixtures

--- src/test/java/unittest/fixtures/TransactionFixture.java
+++ src/test/java/unittest/fixtures/TransactionFixture.java
@@ -12,20 +12,9 @@ public class TransactionFixture extends Transaction {
     private static final Random RANDOM = new Random();
 
     /**
-     * Constructor of the transaction.
-     *
-     * @param referenceBlockId identifier of a finalized block that this transaction refers to its snapshot.
-     * @param sender           identifier of the sender of this transaction.
-     * @param receiver         identifier of the receiver of this transaction.
-     * @param amount           amount of LightChain tokens that this transaction transfers from sender to receiver.
-     */
-    public TransactionFixture(Identifier referenceBlockId, Identifier sender, Identifier receiver, double amount) {
-        super(referenceBlockId, sender, receiver, amount);
-    }
-
-    /**
      * Creates a new transaction with randomly created sender identifier, receiver identifier, reference block id,
      * and transaction amount.
+     *
      * @return random Transaction object
      */
     public static model.lightchain.Transaction newTransaction() {
@@ -39,6 +28,7 @@ public class TransactionFixture extends Transaction {
     /**
      * Creates a new transaction with randomly created sender identifier, receiver identifier, reference block id,
      * and given transaction amount.
+     *
      * @param amount Transaction amount for the random Transaction object
      * @return random Transaction object
      */
