e1f97405e445fac7c44a35d49856ad3da61d76ee
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2022-03-06 06:48:22 -0800

    fixes spotbugs

--- src/test/java/unittest/fixtures/TransactionFixture.java
+++ src/test/java/unittest/fixtures/TransactionFixture.java
@@ -5,11 +5,22 @@ import model.lightchain.Transaction;
 
 import java.util.Random;
 public class TransactionFixture extends Transaction {
-
     /**
      * Random object to create random amounts.
      */
     private static final Random RANDOM = new Random();
+
+    /**
+     * Constructor of the transaction.
+     *
+     * @param referenceBlockId identifier of a finalized block that this transaction refers to its snapshot.
+     * @param sender           identifier of the sender of this transaction.
+     * @param receiver         identifier of the receiver of this transaction.
+     * @param amount           amount of LightChain tokens that this transaction transfers from sender to receiver.
+     */
+    public TransactionFixture(Identifier referenceBlockId, Identifier sender, Identifier receiver, double amount) {
+        super(referenceBlockId, sender, receiver, amount);
+    }
 
     /**
      * Creates a new transaction with randomly created sender identifier, receiver identifier, reference block id,
