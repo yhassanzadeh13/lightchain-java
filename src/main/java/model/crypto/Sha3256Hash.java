2c95068db3c1dea491e597a941a2fb5e30497d5a
Özgün Ozan Nacitarhan <72516041+onacitarhan17@users.noreply.github.com>
2022-03-08 17:29:51 +0300

    Ozan/json encoder and sha3256 hashing (#6)
    * Adds encoder and decoder but gets an error during tests
    
    * adds JSON encoding and decoding + its tests
    
    * print change in JsonEncoderTest
    
    * deletes unused import
    
    * integrates SHA3-256 and JSON encoding
    
    * resolves checkstyle and spotbug issues
    
    * deletes unused import
    
    * handles exceptions properly
    
    * adds sanity checks for Sha3256Hash
    
    * adds requested changes
    
    * changes test names in Sha3256HasherTest.java
    
    * refactors the implementation of JSON encoding/decoding
    
    * deletes unused toString() function
    
    * solves the variable problem in Sha3256hasher.java
    
    * refactors exception messages
    
    * solves the problems in the EntitiyFixture.java and adds Sha3256HashFixture class
    
    * removes id overriding for mock entity
    
    Co-authored-by: Yahya Hassanzadeh <yhassanzadeh13@ku.edu.tr>

---
+++ src/main/java/model/crypto/Sha3256Hash.java
@@ -0,0 +1,67 @@
+package model.crypto;
+
+import java.util.Arrays;
+
+import model.lightchain.Identifier;
+
+/**
+ * Represents SHA3-256 data type which extends abstract Hash data type for
+ * the cryptographic hash function used in LightChain.
+ */
+public class Sha3256Hash extends Hash {
+  public static final int Size = 32;
+  private final byte[] hashBytes;
+
+  /**
+   * Constructs a SHA3-256 hash object from a byte array.
+   *
+   * @param hashValue the byte array to construct the hash from
+   */
+  public Sha3256Hash(byte[] hashValue) {
+    super(hashValue);
+    if (hashValue.length != Size) {
+      throw new IllegalArgumentException("hash value must be 32 bytes long");
+    }
+    this.hashBytes = hashValue.clone();
+  }
+
+  /**
+   * Constructs a SHA3-256 hash object from an identifier.
+   *
+   * @param identifier the identifier to construct the hash from
+   */
+  public Sha3256Hash(Identifier identifier) {
+    super(identifier);
+    if (identifier.getBytes().length != Size) {
+      throw new IllegalArgumentException("identifier must be 32 bytes long");
+    }
+    this.hashBytes = identifier.getBytes();
+  }
+
+  @Override
+  public int compare(Hash other) {
+    return this.toIdentifier().comparedTo(other.toIdentifier());
+  }
+
+  @Override
+  public Identifier toIdentifier() {
+    return new Identifier(this.hashBytes);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Sha3256Hash that = (Sha3256Hash) o;
+    return Arrays.equals(hashBytes, that.hashBytes);
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(hashBytes);
+  }
+}
--- src/test/java/unittest/fixtures/EntityFixture.java
+++ src/test/java/unittest/fixtures/EntityFixture.java
@@ -1,16 +1,35 @@
 package unittest.fixtures;
 
 import model.Entity;
+import model.crypto.Sha3256Hash;
 import model.lightchain.Identifier;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
 
 public class EntityFixture extends Entity {
   private static final String TYPE_FIXTURE_ENTITY = "fixture-entity-type";
+  private static final Random rand = new Random();
   private final Identifier id;
+  private final int testInt;
+  private final String testString;
+  private final double testDouble;
+  private final byte[] testBytes;
+  private final Sha3256Hash[] testHashArray;
 
   public EntityFixture() {
     super();
     this.id = IdentifierFixture.NewIdentifier();
+    this.testInt = rand.nextInt();
+    byte[] bytesString = new byte[32];
+    rand.nextBytes(bytesString);
+    this.testString = new String(bytesString);
+    this.testDouble = rand.nextDouble();
+    this.testBytes = Bytes.ByteArrayFixture(32).clone();
+    this.testHashArray = Sha3256HashFixture.NewSha3256HashArray();
   }
+
 
   @Override
   public String type() {
@@ -18,7 +37,20 @@ public class EntityFixture extends Entity {
   }
 
   @Override
-  public Identifier id() {
-    return this.id;
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    EntityFixture that = (EntityFixture) o;
+    return testInt == that.testInt && Double.compare(that.testDouble, testDouble) == 0
+            && id.comparedTo(that.id) == 0 && testString.equals(that.testString)
+            && Arrays.equals(testBytes, that.testBytes) && Arrays.equals(testHashArray, that.testHashArray);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = Objects.hash(id, testInt, testString, testDouble);
+    result = 31 * result + Arrays.hashCode(testBytes);
+    result = 31 * result + Arrays.hashCode(testHashArray);
+    return result;
   }
 }
