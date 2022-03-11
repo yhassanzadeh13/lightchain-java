16ffb2aaec465329cd54f5130db9ae7dc6876783
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2022-03-11 06:49:50 -0800

    adds snapshot tests

---
+++ src/test/java/state/table/TableStateTest.java
@@ -0,0 +1,9 @@
+package state.table;
+
+public class TableStateTest {
+  // TODO: implementing a test covering each of the following scenarios.
+  // 1. create a state with 10 snapshots. Each snapshot containing with 10 staked and 10 unstaked accounts. Then check
+  //    each snapshot is retrievable using its block id, and that all accounts of the snapshot
+  //    are retrievable using the getter.
+  //    also, check the last() method of state correctly returns the snapshot with the max block height. 
+}
