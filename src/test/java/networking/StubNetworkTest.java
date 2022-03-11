7c7479a9d8b89531ba5f0ef3d42c9674a015a463
akucukoduk16 <akucukoduk16@ku.edu.tr>
2022-03-11 16:13:02 +0300

    TestUnicastOneToAll_Sequentially implemented

--- src/test/java/networking/StubNetworkTest.java
+++ src/test/java/networking/StubNetworkTest.java
@@ -22,12 +22,45 @@ public class StubNetworkTest {
     // receives messages destinated for it (C receives all messages from A) and (D receives all messages from B). Note that A and C must be on the same channel, and B
     // and B must be on another same channel.
     // 5. Stub network throws an exception if an engine is registering itself on an already taken channel.
+    Hub hub = new Hub();
+    StubNetwork stubNetwork1 = new StubNetwork(hub);
+    StubNetwork stubNetwork2 = new StubNetwork(hub);
+    StubNetwork stubNetwork3 = new StubNetwork(hub);
+    StubNetwork stubNetwork4 = new StubNetwork(hub);
+    StubNetwork stubNetwork5 = new StubNetwork(hub);
+    StubNetwork stubNetwork6 = new StubNetwork(hub);
+    StubNetwork stubNetwork7 = new StubNetwork(hub);
+    StubNetwork stubNetwork8 = new StubNetwork(hub);
+    StubNetwork stubNetwork9 = new StubNetwork(hub);
+    StubNetwork stubNetwork10 = new StubNetwork(hub);
+    String channel1="test-network-channel-1";
+    String channel2="test-network-channel-2";
+    Engine A1 = new MockEngine();
+    Engine A2 = new MockEngine();
+    Engine B1 = new MockEngine();
+    Engine B2 = new MockEngine();
+    Engine C1 = new MockEngine();
+    Engine C2 = new MockEngine();
+    Engine D1 = new MockEngine();
+    Engine D2 = new MockEngine();
+    Engine E1 = new MockEngine();
+    Engine E2 = new MockEngine();
+    Engine F1 = new MockEngine();
+    Engine F2 = new MockEngine();
+    Engine G1 = new MockEngine();
+    Engine G2 = new MockEngine();
+    Engine H1 = new MockEngine();
+    Engine H2 = new MockEngine();
+    Engine I1 = new MockEngine();
+    Engine I2 = new MockEngine();
+    Engine J1 = new MockEngine();
+    Engine J2 = new MockEngine();
+
 
     @Test
+
     void test() throws LightChainNetworkingException {
-        Hub hub = new Hub();
-        StubNetwork stubNetwork1 = new StubNetwork(hub);
-        StubNetwork stubNetwork2 = new StubNetwork(hub);
+
         String channelA1="ChannelA1";
 
         Engine A1 = new MockEngine();
@@ -41,5 +74,49 @@ public class StubNetworkTest {
         //c1.unicast(e1, stubNetwork2.id());
         System.out.println(e1.id());
         System.out.println(B1);
+    }
+
+    @Test
+    void TestUnicastOneToAll_Sequentially() throws LightChainNetworkingException {
+        Conduit c1 =stubNetwork1.register(A1,channel1);
+        EntityFixture e1 =new EntityFixture();
+
+        stubNetwork1.register(A2,channel2);
+        stubNetwork2.register(B1,channel1);
+        stubNetwork2.register(B2,channel2);
+        stubNetwork3.register(C1,channel1);
+        stubNetwork3.register(C2,channel2);
+        stubNetwork4.register(D1,channel1);
+        stubNetwork4.register(D2,channel2);
+        stubNetwork5.register(E1,channel1);
+        stubNetwork5.register(E2,channel2);
+        stubNetwork6.register(F1,channel1);
+        stubNetwork6.register(F2,channel2);
+        stubNetwork7.register(G1,channel1);
+        stubNetwork7.register(G2,channel2);
+        stubNetwork8.register(H1,channel1);
+        stubNetwork8.register(H2,channel2);
+        stubNetwork9.register(I1,channel1);
+        stubNetwork9.register(I2,channel2);
+        stubNetwork10.register(J1,channel1);
+        stubNetwork10.register(J2,channel2);
+        c1.unicast(e1,stubNetwork2.id());
+        c1.unicast(e1,stubNetwork3.id());
+        c1.unicast(e1,stubNetwork4.id());
+        c1.unicast(e1,stubNetwork5.id());
+        c1.unicast(e1,stubNetwork6.id());
+        c1.unicast(e1,stubNetwork7.id());
+        c1.unicast(e1,stubNetwork8.id());
+        c1.unicast(e1,stubNetwork9.id());
+        c1.unicast(e1,stubNetwork10.id());
+        System.out.println(e1.id());
+        System.out.println(B1);
+        System.out.println(B2);
+        System.out.println(C1);
+        System.out.println(C2);
+        System.out.println(D1);
+        System.out.println(D2);
+
+
     }
 }
