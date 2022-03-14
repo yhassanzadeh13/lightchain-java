ef76c4e5e0649b6fbce825111a29185f85f1a989
İrem Şahin <64289245+saarantrass@users.noreply.github.com>
2022-03-14 16:07:51 +0300

    Transaction Verification implementation and Verification Tests (#9)
    * tryout commit
    
    * verifier and validator test partly done
    
    * implements transaction verifier for correctness
    
    * TransactionVerifier, validation tests and fixtures added.
    
    * Revert "TransactionVerifier, validation tests and fixtures added."
    
    This reverts commit 7c2b0d8111f54e219d325840325b6a91bf53f843.
    
    * TransactionVerifier, validation tests, and fixtures are added.
    
    * checkstyle errors partly corrected
    
    * spotbug fixes
    
    * more spotbug fix
    
    * checkstyle bugs fixed
    
    * updates gitignore
    
    * reverts changes into account
    
    * simplifies transaction verifier
    
    * updates gitignore
    
    * removes vscode
    
    * fixes compile error on account fixture
    
    * cleans up transaction fixtures
    
    * fixes spotbugs
    
    * fixes a comment
    
    * fixes lint
    
    * fixes lint
    
    * fixes lint
    
    * cleans up validator test
    
    * fixes spotbugs
    
    * fixes test naming and sanity checks
    
    * renames validator
    
    * adds fixture sanity check
    
    Co-authored-by: yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>

--- src/test/java/unittest/fixtures/IdentifierFixture.java
+++ src/test/java/unittest/fixtures/IdentifierFixture.java
@@ -1,8 +1,8 @@
 package unittest.fixtures;
 
 public class IdentifierFixture {
-  public static model.lightchain.Identifier NewIdentifier() {
-    byte[] bytes = Bytes.ByteArrayFixture(model.lightchain.Identifier.Size);
+  public static model.lightchain.Identifier newIdentifier() {
+    byte[] bytes = Bytes.byteArrayFixture(model.lightchain.Identifier.Size);
     return new model.lightchain.Identifier(bytes);
   }
 }
