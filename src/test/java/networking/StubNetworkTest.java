0878ecdfdc69de20dc0bdb1a323ad8b46d3f52df
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2022-03-14 07:35:04 -0700

    adds concurrent test

--- src/test/java/networking/StubNetworkTest.java
+++ src/test/java/networking/StubNetworkTest.java
@@ -3,6 +3,9 @@ package networking;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import model.Entity;
 import model.exceptions.LightChainNetworkingException;
@@ -74,105 +77,56 @@ public class StubNetworkTest {
     Assertions.assertTrue(A2.hasReceived(entity));
   }
 
-
-
-
   @Test
-  void TestUnicastOneToAll_Sequentially() throws LightChainNetworkingException {
+  void TestTwoStubNetworks_TwoEngines_ConcurrentMessages() {
+    int concurrencyDegree = 100;
+    AtomicInteger threadError = new AtomicInteger();
+    CountDownLatch countDownLatch = new CountDownLatch(concurrencyDegree);
+
+    String channel1 = "test-network-channel-1";
+
+    Thread[] unicastThreads = new Thread[concurrencyDegree];
+    Hub hub = new Hub();
+
+    StubNetwork network1 = new StubNetwork(hub);
+    MockEngine A1 = new MockEngine();
+    Conduit c1 = network1.register(A1, channel1);
+
+    StubNetwork network2 = new StubNetwork(hub);
+    MockEngine A2 = new MockEngine();
+    network2.register(A2, channel1);
 
 
+    for(int i = 0; i < concurrencyDegree; i++){
+      unicastThreads[i] = new Thread(() -> {
+        Entity entity = new EntityFixture();
 
-//    Conduit c1 = stubNetwork1.register(A1, channel1);
-//    EntityFixture e1 = new EntityFixture();
-//
-//    stubNetwork1.register(A2, channel2);
-//    stubNetwork2.register(B1, channel1);
-//    stubNetwork2.register(B2, channel2);
-//    stubNetwork3.register(C1, channel1);
-//    stubNetwork3.register(C2, channel2);
-//    stubNetwork4.register(D1, channel1);
-//    stubNetwork4.register(D2, channel2);
-//    stubNetwork5.register(E1, channel1);
-//    stubNetwork5.register(E2, channel2);
-//    stubNetwork6.register(F1, channel1);
-//    stubNetwork6.register(F2, channel2);
-//    stubNetwork7.register(G1, channel1);
-//    stubNetwork7.register(G2, channel2);
-//    stubNetwork8.register(H1, channel1);
-//    stubNetwork8.register(H2, channel2);
-//    stubNetwork9.register(I1, channel1);
-//    stubNetwork9.register(I2, channel2);
-//    stubNetwork10.register(J1, channel1);
-//    stubNetwork10.register(J2, channel2);
-//    c1.unicast(e1, stubNetwork2.id());
-//    c1.unicast(e1, stubNetwork3.id());
-//    c1.unicast(e1, stubNetwork4.id());
-//    c1.unicast(e1, stubNetwork5.id());
-//    c1.unicast(e1, stubNetwork6.id());
-//    c1.unicast(e1, stubNetwork7.id());
-//    c1.unicast(e1, stubNetwork8.id());
-//    c1.unicast(e1, stubNetwork9.id());
-//    c1.unicast(e1, stubNetwork10.id());
-//    System.out.println(e1.id());
-//    System.out.println(B1);
-//    System.out.println(B2);
-//    System.out.println(C1);
-//    System.out.println(C2);
-//    System.out.println(D1);
-//    System.out.println(D2);
+        try {
+          c1.unicast(entity, network2.id());
+          if(!A2.hasReceived(entity)) {
+            threadError.getAndIncrement();
+          }
 
+          countDownLatch.countDown();
+        } catch (LightChainNetworkingException e) {
+          threadError.getAndIncrement();
+        }
+      });
+    }
+
+    for (Thread t: unicastThreads){
+      t.start();
+    }
+
+    try {
+      boolean doneOneTime = countDownLatch.await(60, TimeUnit.SECONDS);
+      Assertions.assertTrue(doneOneTime);
+    } catch (InterruptedException e) {
+      Assertions.fail();
+    }
+
+    Assertions.assertEquals(0, threadError.get());
   }
-//
-//  @Test
-//  void TestUnicastOneToAll_Concurrently() {
-//
-//    EntityFixture e1 = new EntityFixture();
-//    stubNetwork1.register(A1, channel1);
-//    stubNetwork1.register(A2, channel2);
-//    stubNetwork2.register(B1, channel1);
-//    stubNetwork2.register(B2, channel2);
-//    stubNetwork3.register(C1, channel1);
-//    stubNetwork3.register(C2, channel2);
-//    stubNetwork4.register(D1, channel1);
-//    stubNetwork4.register(D2, channel2);
-//    stubNetwork5.register(E1, channel1);
-//    stubNetwork5.register(E2, channel2);
-//    stubNetwork6.register(F1, channel1);
-//    stubNetwork6.register(F2, channel2);
-//    stubNetwork7.register(G1, channel1);
-//    stubNetwork7.register(G2, channel2);
-//    stubNetwork8.register(H1, channel1);
-//    stubNetwork8.register(H2, channel2);
-//    stubNetwork9.register(I1, channel1);
-//    stubNetwork9.register(I2, channel2);
-//    stubNetwork10.register(J1, channel1);
-//    stubNetwork10.register(J2, channel2);
-//    StubNetworkThread t1 = new StubNetworkThread(stubNetwork1, stubNetwork2, e1, channel1);
-//    StubNetworkThread t2 = new StubNetworkThread(stubNetwork1, stubNetwork3, e1, channel1);
-//    StubNetworkThread t3 = new StubNetworkThread(stubNetwork1, stubNetwork4, e1, channel1);
-//    StubNetworkThread t4 = new StubNetworkThread(stubNetwork1, stubNetwork5, e1, channel1);
-//    StubNetworkThread t5 = new StubNetworkThread(stubNetwork1, stubNetwork6, e1, channel1);
-//    StubNetworkThread t6 = new StubNetworkThread(stubNetwork1, stubNetwork7, e1, channel1);
-//    StubNetworkThread t7 = new StubNetworkThread(stubNetwork1, stubNetwork8, e1, channel1);
-//    StubNetworkThread t8 = new StubNetworkThread(stubNetwork1, stubNetwork9, e1, channel1);
-//    StubNetworkThread t9 = new StubNetworkThread(stubNetwork1, stubNetwork10, e1, channel1);
-//
-//    t1.start();
-//    t2.start();
-//    t3.start();
-//    t4.start();
-//    t5.start();
-//    t6.start();
-//    t7.start();
-//    t8.start();
-//    t9.start();
-//    System.out.println(e1.id());
-//    System.out.println(B1);
-//    System.out.println(B2);
-//    System.out.println(C1);
-//    System.out.println(C2);
-//    System.out.println(D1);
-//    System.out.println(D2);
-//
-//  }
+
+
 }
