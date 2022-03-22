ca5ae630756a184cb13222792bf40fefbfb7d3f9
Ozan Nacitarhan <onacitarhan17@ku.edu.tr>
2022-03-22 23:23:38 +0300

    adds improvements to EcdsaPublicKey class

--- src/main/java/model/crypto/ecdsa/EcdsaPublicKey.java
+++ src/main/java/model/crypto/ecdsa/EcdsaPublicKey.java
@@ -17,9 +17,26 @@ import modules.codec.JsonEncoder;
 public class EcdsaPublicKey extends model.crypto.PublicKey {
 
   private static final String SIGN_ALG_SHA_3_256_WITH_ECDSA = "SHA3-256withECDSA";
+  private static final String ELLIPTIC_CURVE = "EC";
+  private final PublicKey ecdsaPublicKey;
 
+  /**
+   * Constructs an Ecdsa public key from the given encoded public key.
+   *
+   * @param bytes encoded public key bytes.
+   */
   public EcdsaPublicKey(byte[] bytes) {
     super(bytes);
+    KeyFactory keyFactory;
+    try {
+      keyFactory = KeyFactory.getInstance(ELLIPTIC_CURVE);
+      EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(this.bytes);
+      ecdsaPublicKey = keyFactory.generatePublic(publicKeySpec);
+    } catch (NoSuchAlgorithmException e) {
+      throw new IllegalStateException(ELLIPTIC_CURVE + "algorithm not found", e);
+    } catch (InvalidKeySpecException e) {
+      throw new IllegalStateException("key spec is invalid", e);
+    }
   }
 
   /**
@@ -31,37 +48,19 @@ public class EcdsaPublicKey extends model.crypto.PublicKey {
    */
   @Override
   public boolean verifySignature(Entity e, model.crypto.Signature s) {
-    Signature ecdsaVerify;
-    KeyFactory keyFactory;
-    PublicKey ecdsaPublicKey;
     try {
-      ecdsaVerify = Signature.getInstance(SIGN_ALG_SHA_3_256_WITH_ECDSA);
-    } catch (NoSuchAlgorithmException ex) {
-      throw new IllegalStateException(SIGN_ALG_SHA_3_256_WITH_ECDSA + " algorithm not found", ex);
-    }
-    try {
-      keyFactory = KeyFactory.getInstance("EC");
-    } catch (NoSuchAlgorithmException ex) {
-      throw new IllegalStateException(SIGN_ALG_SHA_3_256_WITH_ECDSA + " algorithm not found", ex);
-    }
-    EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(this.bytes);
-    try {
-      ecdsaPublicKey = keyFactory.generatePublic(publicKeySpec);
-    } catch (InvalidKeySpecException ex) {
-      throw new IllegalStateException("key spec is invalid", ex);
-    }
-    try {
+      Signature ecdsaVerify = Signature.getInstance(SIGN_ALG_SHA_3_256_WITH_ECDSA);
       ecdsaVerify.initVerify(ecdsaPublicKey);
-    } catch (InvalidKeyException ex) {
-      throw new IllegalStateException("key is invalid", ex);
-    }
-    JsonEncoder encoder = new JsonEncoder();
-    EncodedEntity encodedEntity = encoder.encode(e);
-    Sha3256Hasher hasher = new Sha3256Hasher();
-    Sha3256Hash hash = hasher.computeHash(encodedEntity);
-    try {
+      JsonEncoder encoder = new JsonEncoder();
+      EncodedEntity encodedEntity = encoder.encode(e);
+      Sha3256Hasher hasher = new Sha3256Hasher();
+      Sha3256Hash hash = hasher.computeHash(encodedEntity);
       ecdsaVerify.update(hash.getBytes());
       return ecdsaVerify.verify(s.getBytes());
+    } catch (NoSuchAlgorithmException ex) {
+      throw new IllegalStateException(SIGN_ALG_SHA_3_256_WITH_ECDSA + " algorithm not found", ex);
+    } catch (InvalidKeyException ex) {
+      throw new IllegalStateException("key is invalid", ex);
     } catch (SignatureException ex) {
       throw new IllegalStateException("signature is not initialed correctly", ex);
     }
