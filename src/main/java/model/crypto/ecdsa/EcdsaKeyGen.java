bec1649a4c5555d97c3c7a0bd7db708d70e1c705
Ozan Nacitarhan <onacitarhan17@ku.edu.tr>
2022-03-22 18:26:27 +0300

    removes punctuations in the exception messages

--- src/main/java/model/crypto/ecdsa/EcdsaKeyGen.java
+++ src/main/java/model/crypto/ecdsa/EcdsaKeyGen.java
@@ -24,12 +24,12 @@ public class EcdsaKeyGen implements KeyGen {
     try {
       g = KeyPairGenerator.getInstance("EC");
     } catch (NoSuchAlgorithmException e) {
-      throw new IllegalStateException(SIGN_ALG_SHA_3_256_WITH_ECDSA + " algorithm not found.", e);
+      throw new IllegalStateException(SIGN_ALG_SHA_3_256_WITH_ECDSA + " algorithm not found", e);
     }
     try {
       g.initialize(ecSpec, new SecureRandom());
     } catch (InvalidAlgorithmParameterException e) {
-      throw new IllegalStateException(ellipticCurve + " algorithm parameter not found.", e);
+      throw new IllegalStateException(ellipticCurve + " algorithm parameter not found", e);
     }
     KeyPair keypair = g.generateKeyPair();
     PublicKey publicKey = keypair.getPublic();
