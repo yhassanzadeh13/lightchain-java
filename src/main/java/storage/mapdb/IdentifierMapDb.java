f3377d9372d0772ef9809d6c083b4595871c1ea2
akucukoduk16 <abdullahkucukodu@gmail.com>
2022-03-27 19:24:08 +0300

    Overridden methods are added

--- src/main/java/storage/mapdb/IdentifierMapDb.java
+++ src/main/java/storage/mapdb/IdentifierMapDb.java
@@ -1,4 +1,52 @@
 package storage.mapdb;
 
-public class IdentifierMapDb {
+import model.lightchain.Identifier;
+import storage.Identifiers;
+import org.mapdb.*;
+import java.util.ArrayList;
+
+public class IdentifierMapDb implements Identifiers {
+
+  /**
+   * Adds an identifier to the storage, returns true if it is new, false if it already exists.
+   *
+   * @param identifier identifier to be added to storage.
+   * @return true if it is new, false if it already exists.
+   */
+  @Override
+  public boolean add(Identifier identifier) {
+    return false;
+  }
+
+  /**
+   * Checks existence of an identifier on the storage.
+   *
+   * @param identifier identifier to be checked.
+   * @return true if identifier exists on the storage, false otherwise.
+   */
+  @Override
+  public boolean has(Identifier identifier) {
+    return false;
+  }
+
+  /**
+   * Removes an identifier from the storage.
+   *
+   * @param identifier identifier to be removed.
+   * @return true if identifier exists and removed, false otherwise.
+   */
+  @Override
+  public boolean remove(Identifier identifier) {
+    return false;
+  }
+
+  /**
+   * Returns all stored identifiers on storage.
+   *
+   * @return all stored identifiers on storage.
+   */
+  @Override
+  public ArrayList<Identifier> all() {
+    return null;
+  }
 }
