0f7c8c763bbdf273e48cc7fd565a131c933cd2d1
akucukoduk16 <abdullahkucukodu@gmail.com>
2022-03-28 16:59:22 +0300

    IdentifierMapDb first test implemented

--- src/test/java/storage/IdentifiersTest.java
+++ src/test/java/storage/IdentifiersTest.java
@@ -1,12 +1,18 @@
 package storage;
 
+import model.lightchain.Identifier;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mapdb.DB;
+import storage.mapdb.IdentifierMapDb;
+import unittest.fixtures.IdentifierFixture;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /**
  * Encapsulates tests for identifiers database.
@@ -31,21 +37,38 @@ public class IdentifiersTest {
   //    Has returns true for each of them, and All returns list of all of them. Then try Adding all of them again, and
   //    Add should return false for each of them.
 
+
   /**
    * When adding 10 new identifiers sequentially, the Add method must return true for all of them.
-   *
    */
   @Test
-  void firstTest() {
-
-    try {
-      Path tempdir = Files.createTempDirectory( "tmp");
-      Files.createTempFile(tempdir,"tempfile",".db");
-    } catch (IOException e) {
-      throw new IllegalArgumentException("could not be tempdir with this prefix"+e);
+  void firstTest() throws IOException {
+    int count=0;
+    Path currentRelativePath = Paths.get("");
+    String s = currentRelativePath.toAbsolutePath().toString();
+    System.out.println("Current absolute path is: " + s);
+    ArrayList<Identifier> identifierArrayList = new ArrayList<>();
+    for (int i = 0; i < 9; i++) {
+      Identifier identifier = IdentifierFixture.newIdentifier();
+      identifierArrayList.add(identifier);
     }
+    Path tempdir = Files.createTempDirectory(currentRelativePath, "tempdir");
+    Path tempfile = Files.createTempFile(tempdir, "tempfile", ".db");
+    System.out.println(tempfile.toString());
+    IdentifierMapDb db = new IdentifierMapDb(s + tempdir.toString() + tempdir.toString());
 
+    for (Identifier identifier : identifierArrayList) {
+      if (!db.add(identifier)) {
+        count++;
+      }
+    }
+    for (Identifier identifier : identifierArrayList) {
+      if (!db.has(identifier)) {
+        count++;
+      }
+    }
+    Assertions.assertEquals(0, count);
 
-
+    tempdir.toFile().deleteOnExit();
   }
 }
