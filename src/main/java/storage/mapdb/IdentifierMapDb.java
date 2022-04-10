45ff70dcce0d3601bd42aac10917a0e1aa08dd4f
abdullah küçüködük <68288042+akucukoduk16@users.noreply.github.com>
2022-04-10 16:34:01 +0300

    Mapdb identifiers (#26)
    * IdentifierMapDb is added
    
    * Overridden methods are added
    
    * IdentifierMapDb is implemented
    
    * IdentifierMapDb modification
    
    * IdentifierMapDb first test implemented
    
    * adds maven dependency and temp directory delete
    
    * wip
    
    * IdentifierMapDb is added
    
    * Overridden methods are added
    
    * IdentifierMapDb is implemented
    
    * IdentifierMapDb modification
    
    * IdentifierMapDb first test implemented
    
    * adds maven dependency and temp directory delete
    
    * wip
    
    * IdentifierMapDb is added
    
    * Overridden methods are added
    
    * IdentifierMapDb is implemented
    
    * IdentifierMapDb modification
    
    * IdentifierMapDb first test implemented
    
    * wip
    
    * IdentifierMapDb and its tests implemented
    
    * Overridden methods are added
    
    * IdentifierMapDb is implemented
    
    * IdentifierMapDb modification
    
    * IdentifierMapDb first test implemented
    
    * adds maven dependency and temp directory delete
    
    * wip
    
    * IdentifierMapDb and its tests implemented
    
    * IdentifierMapDb and its tests implemented
    
    * Some CodeStyle modifications
    
    * Some CodeStyle modifications
    
    * Some CodeStyle modifications
    
    * Some CodeStyle modifications
    
    * Some CodeStyle modifications
    
    * Some Access modifier changed
    
    * Some Access modifier changed
    
    * wip
    
    * wip
    
    * fixes lint
    
    * Implement sequential tests
    
    * Implement sequential tests
    
    * Implement sequential tests
    
    * Implement sequential tests
    
    * concurrentAddTest is implemented
    
    * lock fix
    
    * whitespace fix
    
    * lock fix
    
    * concurrentRemoveFirstFiveTest implemented
    
    * concurrentRemoveFirstFiveTest implemented
    
    * concurrentDuplicationTest is implemented.
    
    * applies revisions
    
    * fixes lints
    
    Co-authored-by: akucukoduk16 <abdullahkucukodu@gmail.com>
    Co-authored-by: Yahya <yhassanzadeh13@ku.edu.tr>

---
+++ src/main/java/storage/mapdb/IdentifierMapDb.java
@@ -0,0 +1,106 @@
+package storage.mapdb;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import model.lightchain.Identifier;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
+import storage.Identifiers;
+
+/**
+ * Implementation of identifiers interface.
+ */
+public class IdentifierMapDb implements Identifiers {
+  private final DB db;
+  private final ReentrantReadWriteLock lock;
+  private static final String MAP_NAME = "identifier_map";
+  private final HTreeMap<byte[], byte[]> identifierMap;
+
+  /**
+   * Creates MapDb.
+   *
+   * @param filePath the path of the file.
+   */
+  public IdentifierMapDb(String filePath) {
+    this.db = DBMaker.fileDB(filePath).make();
+    this.lock = new ReentrantReadWriteLock();
+    identifierMap = this.db.hashMap(MAP_NAME)
+        .keySerializer(Serializer.BYTE_ARRAY)
+        .valueSerializer(Serializer.BYTE_ARRAY)
+        .createOrOpen();
+  }
+
+  /**
+   * Adds an identifier to the storage, returns true if it is new, false if it already exists.
+   *
+   * @param identifier identifier to be added to storage.
+   * @return true if it is new, false if it already exists.
+   */
+  @Override
+  public boolean add(Identifier identifier) {
+    boolean addBoolean;
+    try {
+      lock.writeLock().lock();
+      addBoolean = identifierMap.putIfAbsentBoolean(identifier.getBytes(), identifier.getBytes());
+    } finally {
+      lock.writeLock().unlock();
+    }
+    return addBoolean;
+  }
+
+  /**
+   * Checks existence of an identifier on the storage.
+   *
+   * @param identifier identifier to be checked.
+   * @return true if identifier exists on the storage, false otherwise.
+   */
+  @Override
+  public boolean has(Identifier identifier) {
+    boolean hasBoolean;
+    try {
+      lock.readLock().lock();
+      hasBoolean = identifierMap.containsKey(identifier.getBytes());
+    } finally {
+      lock.readLock().unlock();
+    }
+
+    return hasBoolean;
+  }
+
+  /**
+   * Removes an identifier from the storage.
+   *
+   * @param identifier identifier to be removed.
+   * @return true if identifier exists and removed, false otherwise.
+   */
+  @Override
+  public boolean remove(Identifier identifier) {
+    return identifierMap.remove(identifier.getBytes(), identifier.getBytes());
+  }
+
+  /**
+   * Returns all stored identifiers on storage.
+   *
+   * @return all stored identifiers on storage.
+   */
+  @Override
+  public ArrayList<Identifier> all() {
+    ArrayList<Identifier> arrayList = new ArrayList<>();
+    for (byte[] element : identifierMap.keySet()) {
+      Identifier identifierFromBytes = new Identifier(element);
+      arrayList.add(identifierFromBytes);
+    }
+    return arrayList;
+  }
+
+  /**
+   * It closes the database.
+   */
+  public void closeDb() {
+    db.close();
+  }
+}
+
