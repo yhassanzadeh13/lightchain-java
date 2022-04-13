8907f52825ee756486f325f3adab62b661f511ce
Yahya <yhassanzadeh13@ku.edu.tr>
2022-04-13 07:31:17 -0700

    wip

--- src/test/java/protocol/engines/ValidatorEngineTest.java
+++ src/test/java/protocol/engines/ValidatorEngineTest.java
@@ -1,5 +1,20 @@
 package protocol.engines;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import model.lightchain.ValidatedBlock;
+import model.local.Local;
+import network.Conduit;
+import network.Network;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import state.State;
+
 /**
  * Encapsulates tests for validator engine.
  */
@@ -30,4 +45,44 @@ public class ValidatorEngineTest {
   //     soundness, etc). Invalid transaction should be discarded without sending back a signature to its sender.
   // 19. Unhappy path of receiving a invalid block (one test per each validation criteria, e.g., correctness,
   //     soundness, etc). Invalid block should be discarded without sending back a signature to its proposer.
+
+  @Test
+  public void testReceiveTwoValidBlockConcurrently() {
+    Network net = mock(Network.class);
+    Conduit conduit = mock(Conduit.class);
+    State state = mock(State.class);
+    Local local = new Local();
+
+
+    ValidatorEngine engine = new ValidatorEngine(net, local, state);
+    when(net.register(engine, "validator")).then()
+
+    ValidatedBlock b = null;
+
+    AtomicInteger threadErrorCount = new AtomicInteger();
+    CountDownLatch done = new CountDownLatch(1);
+
+    
+    Thread t = new Thread(() -> {
+      // implement body of thread.
+      // if some error happens that leads to test failure:
+      // threadErrorCount.getAndIncrement();
+      engine.process(b);
+      done.countDown();
+    });
+
+    // run threads
+    t.start();
+
+    try {
+      boolean doneOnTime = done.await(1, TimeUnit.SECONDS);
+      Assertions.assertTrue(doneOnTime);
+    } catch (InterruptedException e) {
+      Assertions.fail(e);
+    }
+
+    Assertions.assertEquals(0, threadErrorCount.get());
+
+
+  }
 }
