5dca52c956851d75ed822d075d617d5250ad25e4
mgulsun18 <mgulsun18@ku.edu.tr>
2022-04-24 15:48:10 +0300

    implements automated metric tracking for arbitrary number of nodesnode

--- src/main/java/bootstrap/Bootstrap.java
+++ src/main/java/bootstrap/Bootstrap.java
@@ -1,9 +1,14 @@
 package bootstrap;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+import com.google.gson.Gson;
 import integration.localnet.LocalTestNet;
 import model.lightchain.Identifier;
 
@@ -13,6 +18,7 @@ import model.lightchain.Identifier;
 public class Bootstrap {
 
   static final String OUTPUT_PATH = "bootstrap.txt";
+  static final String PROM_CONFIG_PATH = "prometheus/nodes.json";
 
   /**
    * Main method.
@@ -39,10 +45,23 @@ public class Bootstrap {
         System.out.println(id.getKey().toString() + " " + idTable.get(id.getKey()));
       }
 
+      writeTargetJSON(idTableModifierForMetricsServerPort(idTable));
+
     } catch (IllegalStateException e) {
       System.err.println("could not initialize and run the Node network: " + e.getMessage());
       System.exit(1);
     }
+
+  }
+
+  private static ConcurrentMap<Identifier, String> idTableModifierForMetricsServerPort(ConcurrentMap<Identifier, String> idTable) {
+    ConcurrentMap modifiedMap = new ConcurrentHashMap<Identifier, String>();
+
+    for (Map.Entry<Identifier, String> id : idTable.entrySet()) {
+      modifiedMap.put(id.getKey(), idTable.get(id.getKey()).split(":")[0] + ":8082");
+    }
+
+    return modifiedMap;
 
   }
 
@@ -60,6 +79,35 @@ public class Bootstrap {
     } catch (IOException e) {
       System.err.println("could open/close the file: " + e);
       System.exit(1);
+    }
+
+  }
+
+  private static void writeTargetJSON(ConcurrentMap<Identifier, String> idTable) {
+
+    try {
+      BufferedWriter writer = Files.newBufferedWriter(Paths.get(PROM_CONFIG_PATH));
+
+      Map<String, Object> nodes = new HashMap<>();
+
+      Map<String, Object> labels = new HashMap<>();
+      labels.put("job", "nodes");
+      labels.put("env", "dev");
+
+      nodes.put("targets", idTable.values());
+      nodes.put("labels", labels);
+
+      Gson gson = new Gson();
+
+      Map[] pm = new Map[1];
+      pm[0] = nodes;
+
+      writer.write(gson.toJson(pm));
+
+      writer.close();
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
 
   }
