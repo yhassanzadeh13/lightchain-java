d4a77a776b5172810349266d471c17d27e43b97d
Yahya <yhassanzadeh13@ku.edu.tr>
2022-04-24 07:07:33 +0400

    applies revisions

--- src/test/java/modules/ads/MerkleTreeTest.java
+++ src/test/java/modules/ads/MerkleTreeTest.java
@@ -10,10 +10,12 @@ import model.crypto.Sha3256Hash;
 import model.lightchain.Identifier;
 import modules.ads.merkletree.AuthenticatedEntityVerifier;
 import modules.ads.merkletree.MerkleTree;
+import modules.ads.merkletree.Proof;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import unittest.fixtures.EntityFixture;
 import unittest.fixtures.MerkleTreeFixture;
+import unittest.fixtures.Sha3256HashFixture;
 
 /**
  * Encapsulates tests for an authenticated and concurrent implementation of MerkleTree ADS.
@@ -38,7 +40,7 @@ public class MerkleTreeTest {
   }
 
   /**
-   * Tests both if putting and getting the same entity gives same proof
+   * Tests both putting and getting the same entity gives same proof
    * and putting another entity gives different proofs.
    */
   @Test
@@ -72,45 +74,61 @@ public class MerkleTreeTest {
   }
 
   /**
-   * Tests if putting an existing entity does not change the proof.
+   * Tests putting an existing entity does not change the proof.
    */
   @Test
   public void testPutExistingEntity() {
     MerkleTree merkleTree = MerkleTreeFixture.createMerkleTree(5);
+    Assertions.assertEquals(merkleTree.size(), 5); // fixture sanity check.
     Entity entity = new EntityFixture();
+
+    // first time put
     AuthenticatedEntity authenticatedEntityPut = merkleTree.put(entity);
     MembershipProof proofPut = authenticatedEntityPut.getMembershipProof();
+    Assertions.assertEquals(merkleTree.size(), 6);
+
+    // second attempt
     AuthenticatedEntity authenticatedEntityPutAgain = merkleTree.put(entity);
     MembershipProof proofPutAgain = authenticatedEntityPutAgain.getMembershipProof();
+
+    // proofs must be equal.
     Assertions.assertEquals(proofPut, proofPutAgain);
+    Assertions.assertEquals(merkleTree.size(), 6); // duplicate entity should not change the size.
   }
 
   /**
-   * Concurrently puts and gets entities and checks if their proofs are correct (thread safety check).
+   * Concurrently puts and gets entities and checks their proofs are correct (thread safety check).
    */
   @Test
   public void testConcurrentPutGet() {
     int concurrencyDegree = 100;
     ArrayList<Entity> entities = new ArrayList<>();
     ArrayList<Identifier> ids = new ArrayList<>();
+
     AtomicInteger threadError = new AtomicInteger();
-    CountDownLatch countDownLatchPut = new CountDownLatch(concurrencyDegree);
-    CountDownLatch countDownLatchGet = new CountDownLatch(concurrencyDegree);
+    CountDownLatch putDone = new CountDownLatch(concurrencyDegree);
+    CountDownLatch getDone = new CountDownLatch(concurrencyDegree);
+
     Thread[] putThreads = new Thread[concurrencyDegree];
     Thread[] getThreads = new Thread[concurrencyDegree];
-    MerkleTree merkleTree = MerkleTreeFixture.createMerkleTree(5);
+
+    MerkleTree merkleTree = MerkleTreeFixture.createMerkleTree(0);
+    Assertions.assertEquals(merkleTree.size(), 0); // fixture sanity check.
+
     for (int i = 0; i < concurrencyDegree; i++) {
       Entity entity = new EntityFixture();
       entities.add(entity);
       ids.add(entity.id());
     }
+
+    // put
     for (int i = 0; i < concurrencyDegree; i++) {
       Entity entity = entities.get(i);
       putThreads[i] = new Thread(() -> {
         try {
           merkleTree.put(entity);
-          countDownLatchPut.countDown();
-        } catch (NullPointerException e) {
+          putDone.countDown();
+        } catch (Exception e) {
           threadError.getAndIncrement();
         }
       });
@@ -119,11 +137,13 @@ public class MerkleTreeTest {
       t.start();
     }
     try {
-      boolean doneOneTimePut = countDownLatchPut.await(60, TimeUnit.SECONDS);
+      boolean doneOneTimePut = putDone.await(60, TimeUnit.SECONDS);
       Assertions.assertTrue(doneOneTimePut);
     } catch (InterruptedException e) {
       Assertions.fail();
     }
+
+    // get
     for (int i = 0; i < concurrencyDegree; i++) {
       Identifier id = ids.get(i);
       getThreads[i] = new Thread(() -> {
@@ -133,8 +153,8 @@ public class MerkleTreeTest {
           if (!verifier.verify(authenticatedEntity)) {
             threadError.getAndIncrement();
           }
-          countDownLatchGet.countDown();
-        } catch (NullPointerException e) {
+          getDone.countDown();
+        } catch (Exception e) {
           threadError.getAndIncrement();
         }
       });
@@ -143,79 +163,110 @@ public class MerkleTreeTest {
       t.start();
     }
     try {
-      boolean doneOneTimeGet = countDownLatchGet.await(60, TimeUnit.SECONDS);
+      boolean doneOneTimeGet = getDone.await(60, TimeUnit.SECONDS);
       Assertions.assertTrue(doneOneTimeGet);
     } catch (InterruptedException e) {
       Assertions.fail();
     }
     Assertions.assertEquals(0, threadError.get());
+    Assertions.assertEquals(concurrencyDegree, merkleTree.size());
   }
 
   /**
-   * Tests if getting an entity that does not exist in the merkle tree throws IllegalArgumentException.
+   * Tests getting an entity that does not exist in the merkle tree throws IllegalArgumentException.
    */
   @Test
   public void testGetNonExistingEntity() {
     MerkleTree merkleTree = MerkleTreeFixture.createMerkleTree(5);
+    Assertions.assertEquals(merkleTree.size(), 5); // fixture sanity check.
     Entity entity = new EntityFixture();
+
     Assertions.assertThrows(IllegalArgumentException.class, () -> {
       merkleTree.get(entity.id());
     });
   }
 
   /**
-   * Tests if inserting null throws IllegalArgumentException.
+   * Tests inserting null throws IllegalArgumentException.
    */
   @Test
   public void testNullInsertion() {
     MerkleTree merkleTree = MerkleTreeFixture.createMerkleTree(5);
+    Assertions.assertEquals(merkleTree.size(), 5); // fixture sanity check.
     Assertions.assertThrows(IllegalArgumentException.class, () -> {
       merkleTree.put(null);
     });
   }
 
   /**
-   * Tests if the proof verifies when root is changed.
+   * Tests the proof verification fails when root is changed.
    */
   @Test
   public void testManipulatedRoot() {
     MerkleTree merkleTree = MerkleTreeFixture.createMerkleTree(5);
+    Assertions.assertEquals(merkleTree.size(), 5); // fixture sanity check.
     Entity entity = new EntityFixture();
     AuthenticatedEntity authenticatedEntity = merkleTree.put(entity);
     MembershipProof proof = authenticatedEntity.getMembershipProof();
-    proof.setRoot(new Sha3256Hash(new byte[32]));
-    authenticatedEntity.setMembershipProof(proof);
+
+    // creates a tampered proof with random root.
+    Proof tamperedProof = new Proof(proof.getPath(), new Sha3256Hash(new byte[32]), proof.getIsLeftNode());
+    AuthenticatedEntity tamperedAuthenticatedEntity = new modules.ads.merkletree.AuthenticatedEntity(
+        tamperedProof,
+        authenticatedEntity.type(),
+        authenticatedEntity.getEntity());
+
+
     AuthenticatedEntityVerifier verifier = new AuthenticatedEntityVerifier();
-    Assertions.assertFalse(verifier.verify(authenticatedEntity));
+    // authenticated entity must be verified.
+    Assertions.assertTrue(verifier.verify(authenticatedEntity));
+    // tampered authenticated entity must be failed.
+    Assertions.assertFalse(verifier.verify(tamperedAuthenticatedEntity));
   }
 
   /**
-   * Tests if the proof verifies when entity is changed.
+   * Tests the proof verification fails when entity is changed.
    */
   @Test
   public void testManipulatedEntity() {
     MerkleTree merkleTree = MerkleTreeFixture.createMerkleTree(5);
+    Assertions.assertEquals(merkleTree.size(), 5); // fixture sanity check.
     Entity entity = new EntityFixture();
     AuthenticatedEntity authenticatedEntity = merkleTree.put(entity);
-    authenticatedEntity.setEntity(new EntityFixture());
+
+    AuthenticatedEntity tamperedEntity = new modules.ads.merkletree.AuthenticatedEntity(
+        (Proof) authenticatedEntity.getMembershipProof(),
+        authenticatedEntity.type(),
+        new EntityFixture());
+
     AuthenticatedEntityVerifier verifier = new AuthenticatedEntityVerifier();
-    Assertions.assertFalse(verifier.verify(authenticatedEntity));
+    Assertions.assertTrue(verifier.verify(authenticatedEntity)); // original authenticated entity passes verification.
+    Assertions.assertFalse(verifier.verify(tamperedEntity)); // tampered entity fails verification.
   }
 
   /**
-   * Tests if the proof verifies when proof is changed.
+   * Tests the proof fails verification when proof part of authenticated entity is changed.
    */
   @Test
   public void testManipulatedProof() {
     MerkleTree merkleTree = MerkleTreeFixture.createMerkleTree(5);
+    Assertions.assertEquals(merkleTree.size(), 5); // fixture sanity check.
+
     Entity entity = new EntityFixture();
     AuthenticatedEntity authenticatedEntity = merkleTree.put(entity);
     MembershipProof proof = authenticatedEntity.getMembershipProof();
-    ArrayList<Sha3256Hash> proofPath = proof.getPath();
-    proofPath.set(0, new Sha3256Hash(new byte[32]));
-    proof.setPath(proofPath);
-    authenticatedEntity.setMembershipProof(proof);
+
+    AuthenticatedEntity tamperedEntity = new modules.ads.merkletree.AuthenticatedEntity(
+        new Proof(Sha3256HashFixture.newSha3256HashArrayList(
+            proof.getPath().size()),
+            proof.getRoot(),
+            proof.getIsLeftNode()),
+        authenticatedEntity.type(),
+        authenticatedEntity.getEntity());
+
+
     AuthenticatedEntityVerifier verifier = new AuthenticatedEntityVerifier();
-    Assertions.assertFalse(verifier.verify(authenticatedEntity));
+    Assertions.assertTrue(verifier.verify(authenticatedEntity)); // original authenticated entity passes verification.
+    Assertions.assertFalse(verifier.verify(tamperedEntity)); // tampered entity fails verification.
   }
 }
