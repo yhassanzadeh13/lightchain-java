094a823e29fec90caa352e363f1da140e764af05
akucukoduk16 <abdullahkucukodu@gmail.com>
2022-05-10 16:58:29 +0300

    Proposer engine test setup

--- src/test/java/protocol/engines/ProposerEngineTest.java
+++ src/test/java/protocol/engines/ProposerEngineTest.java
@@ -1,9 +1,47 @@
 package protocol.engines;
 
+import model.crypto.PrivateKey;
+import model.lightchain.Account;
+import model.lightchain.Block;
+import model.lightchain.Identifier;
+import model.local.Local;
+import network.Channels;
+import network.Network;
+import network.NetworkAdapter;
+import networking.MockConduit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import protocol.block.BlockValidator;
+import state.Snapshot;
+import state.State;
+import storage.Blocks;
+import storage.Transactions;
+import unittest.fixtures.AccountFixture;
+import unittest.fixtures.BlockFixture;
+import unittest.fixtures.IdentifierFixture;
+import unittest.fixtures.KeyGenFixture;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+
 /**
  * Encapsulates tests for the proposer engine.
  */
 public class ProposerEngineTest {
+  private ProposerEngine proposerEngine;
+  private Snapshot snapshot;
+  private Block block1;
+  private State state;
+  private Network network;
+  private Local local;
+  private Blocks blocks;
+  private Transactions pendingTransactions;
+
+  private MockConduit proposedCon;
+  private MockConduit validatedCon;
+  private Identifier localId;
+  private PrivateKey localPrivateKey;
   // TODO: implement following test cases.
   // Mock assigner, state, snapshot, network, and storage components.
   // Create snapshot of 11 staked accounts.
@@ -19,4 +57,45 @@ public class ProposerEngineTest {
   // 5. Proposer engine creates a validated block whenever it receives enough block approvals for its proposed block,
   // it also sends the validated block to all nodes including itself on the expected channel.
   // 6. Extend test case 5 for when proposer engine receives all block approvals concurrently. 
+  @BeforeEach
+  public void setUpMock() {
+
+    localId = IdentifierFixture.newIdentifier();
+    localPrivateKey = KeyGenFixture.newKeyGen().getPrivateKey();
+    local = new Local(localId, localPrivateKey);
+    pendingTransactions = mock(Transactions.class);
+    blocks = mock(Blocks.class);
+    block1 = BlockFixture.newBlock();
+    state = mock(State.class);
+    snapshot = mock(Snapshot.class);
+    network = mock(Network.class);
+    NetworkAdapter networkAdapter = mock(NetworkAdapter.class);
+    proposedCon = new MockConduit(Channels.ProposedBlocks, networkAdapter);
+    validatedCon = new MockConduit(Channels.ValidatedBlocks, networkAdapter);
+    ArrayList<Account>[] a = AccountFixture.newAccounts(11,0).values();
+
+    state = mock(State.class);
+    proposerEngine = new ProposerEngine(blocks,pendingTransactions,state,local,network);
+
+  }
+  @Test
+  public void blockValidationTest(){
+
+    BlockValidator blockValidator = new BlockValidator(state);
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
