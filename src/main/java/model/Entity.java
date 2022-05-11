84b307c547b48e1e2afd6791b4383650723536b5
Özgün Ozan Nacitarhan <72516041+onacitarhan17@users.noreply.github.com>
2022-05-11 17:59:40 +0300

    improves json encoder (#40)
    * improves json encoder
    
    * suppresses lint
    
    * resolves bugs
    
    * supresses CI errors
    
    * deletes redundant implements
    
    * refactors exception handling for encoding
    
    * supress spotbugs
    
    * supress spotbugs
    
    * supress spotbugs
    
    Co-authored-by: Yahya <yhassanzadeh13@ku.edu.tr>

--- src/main/java/model/Entity.java
+++ src/main/java/model/Entity.java
@@ -1,8 +1,12 @@
 package model;
 
+import java.io.Serializable;
+
 import crypto.Sha3256Hasher;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import model.codec.EncodedEntity;
 import model.crypto.Hash;
+import model.exceptions.CodecException;
 import model.lightchain.Identifier;
 import modules.codec.JsonEncoder;
 
@@ -10,15 +14,22 @@ import modules.codec.JsonEncoder;
  * Entity represents the unit of data model in LightChain. Everything meant to be sent over the network, stored
  * in memory, or a database must extend the Entity class.
  */
-public abstract class Entity {
+public abstract class Entity implements Serializable {
   /**
    * Computes the collision resistant hash value of entity.
    *
    * @return identifier representation of hash value for entity.
    */
+  @SuppressFBWarnings(value = "DM_EXIT", justification = "crash node upon failure on encoding")
   public Identifier id() {
     JsonEncoder c = new JsonEncoder();
-    EncodedEntity e = c.encode(this);
+    EncodedEntity e = null;
+    try {
+      e = c.encode(this);
+    } catch (CodecException ex) {
+      System.err.println(ex.getMessage());
+      System.exit(1);
+    }
     Sha3256Hasher h = new Sha3256Hasher();
     Hash hash = h.computeHash(e);
     return hash.toIdentifier();
