8f5c4d531573ca43dccd09b8e328da219ebb24cd
abdullah küçüködük <68288042+akucukoduk16@users.noreply.github.com>
2022-05-23 16:39:34 +0300

    Abdullah/mapdb transactions and blocks (#35)
    * Abdullah/stubnetwork extended test (#20)
    
    * Continue to StubNetwork
    
    * TestUnicastOneToAll_Sequentially implemented
    
    * TestUnicastOneToAll_Concurrently Implemented
    
    * wip
    
    * implements test for two engines
    
    * TestUnicastOneToAll_Concurrently Implemented
    
    * adds concurrent test
    
    * adds lock
    
    * Some tests Implemented
    
    * TestUnicastOneToAll_Concurrently Implemented
    
    * TestUnicastOneToAll_Concurrently Implemented
    
    * deletion of prints
    
    * deletion of prints
    
    * deletion of prints
    
    * Continue to StubNetwork
    
    * TestUnicastOneToAll_Sequentially implemented
    
    * TestUnicastOneToAll_Concurrently Implemented
    
    * wip
    
    * implements test for two engines
    
    * TestUnicastOneToAll_Concurrently Implemented
    
    * adds concurrent test
    
    * adds lock
    
    * Some tests Implemented
    
    * TestUnicastOneToAll_Concurrently Implemented
    
    * deletion of prints
    
    * deletion of prints
    
    * deletion of prints
    
    * rebase
    
    * TestUnicastOneToAll_Sequentially implementations
    
    * TestUnicastOneToSome_Sequentially implementation
    
    * TestUnicastOneToSome_Sequentially implementation
    
    * TestUnicastOneToSome_Concurrently implementation
    
    * TestUnicastOneToAll_Sequentially_TwoEngines implementation
    
    * TestTwoStubNetworks_TwoEngines_Reply_ConcurrentMessages implementation
    
    * TestTwoStubNetworks_FourEngines_ConcurrentMessages implementation
    
    * TestRegisterToOccupiedChannel implementation
    
    * Javadoc modification
    
    * Import reorganize
    
    * Cl changes
    
    * Cl changes
    
    * Cl changes
    
    * applies revisions
    
    * StubNetworkTest is fixed
    
    * StubNetworkTest Identifier fixed.
    
    * applies revision
    
    * applies revisions
    
    * applies revisions
    
    * applies revisions
    
    * fixes lint
    
    * applies revision
    
    * fixes lint
    
    Co-authored-by: yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
    Co-authored-by: akucukoduk16 <abdullahkucukodu@gmail.com>
    
    * TansactionsMapDb and tests are implemented.
    
    * BlocksMapDb is implemented
    
    * BlocksMapDb is implemented but adding problem exist
    
    * Proof of id remains same for transaction
    
    * Change structure
    
    * Change structure
    
    * Transactions and Blocks mapdb done.
    
    * Style modifications
    
    * Style modifications
    
    * Style modifications
    
    * Style modifications
    
    * Style modifications
    
    * Style modifications
    
    * Style modifications
    
    * Style modifications
    
    * applies revisions
    
    * applies revisions
    
    * applies revisions
    
    * applies revisions
    
    * applies revisions
    
    * lint fix
    
    Co-authored-by: yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
    Co-authored-by: akucukoduk16 <abdullahkucukodu@gmail.com>

--- src/main/java/model/lightchain/ValidatedBlock.java
+++ src/main/java/model/lightchain/ValidatedBlock.java
@@ -1,5 +1,7 @@
 package model.lightchain;
 
+import java.util.Arrays;
+
 import model.codec.EntityType;
 import model.crypto.Signature;
 
@@ -34,6 +36,28 @@ public class ValidatedBlock extends Block {
 
   public Signature[] getCertificates() {
     return certificates.clone();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof ValidatedBlock)) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    ValidatedBlock that = (ValidatedBlock) o;
+    return Arrays.equals(getCertificates(), that.getCertificates());
+  }
+
+  @Override
+  public int hashCode() {
+    int result = super.hashCode();
+    result = 31 * result + Arrays.hashCode(getCertificates());
+    return result;
   }
 
   @Override
