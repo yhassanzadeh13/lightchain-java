a3181b25ffbb7026a4415fc684b5e0827db6c096
Yahya <yhassanzadeh13@ku.edu.tr>
2022-06-08 06:59:38 -0700

    applies revisions

--- src/test/java/unittest/fixtures/ValidatedTransactionFixture.java
+++ src/test/java/unittest/fixtures/ValidatedTransactionFixture.java
@@ -44,40 +44,7 @@ public class ValidatedTransactionFixture {
    * @return random ValidatedTransaction object.
    */
   public static ValidatedTransaction newValidatedTransaction(Identifier sender) {
-    Identifier referenceBlockId = IdentifierFixture.newIdentifier();
-    Identifier receiver = IdentifierFixture.newIdentifier();
-    double amount = 100;
-    int certificatesSize = Parameters.SIGNATURE_THRESHOLD;
-    Signature[] certificates = new Signature[certificatesSize];
-    for (int i = 0; i < certificatesSize; i++) {
-      certificates[i] = SignatureFixture.newSignatureFixture();
-    }
-    ValidatedTransaction valTrans = new ValidatedTransaction(referenceBlockId, sender, receiver, amount, certificates);
-    Signature sign = SignatureFixture.newSignatureFixture();
-    valTrans.setSignature(sign);
-    return valTrans;
-  }
-
-  /**
-   * Constructor of the validated transactions with randomly generated parameters
-   * and given sender and receiver identifier.
-   *
-   * @param sender   identifier of the sender of this transaction.
-   * @param receiver identifier of the receiver of this transaction.
-   * @return random ValidatedTransaction object.
-   */
-  public static ValidatedTransaction newValidatedTransaction(Identifier sender, Identifier receiver) {
-    Identifier referenceBlockId = IdentifierFixture.newIdentifier();
-    double amount = 100;
-    int certificatesSize = Parameters.SIGNATURE_THRESHOLD;
-    Signature[] certificates = new Signature[certificatesSize];
-    for (int i = 0; i < certificatesSize; i++) {
-      certificates[i] = SignatureFixture.newSignatureFixture();
-    }
-    ValidatedTransaction valTrans = new ValidatedTransaction(referenceBlockId, sender, receiver, amount, certificates);
-    Signature sign = SignatureFixture.newSignatureFixture();
-    valTrans.setSignature(sign);
-    return valTrans;
+    return newValidatedTransaction(sender, IdentifierFixture.newIdentifier(), Parameters.SIGNATURE_THRESHOLD);
   }
 
   /**
@@ -87,10 +54,9 @@ public class ValidatedTransactionFixture {
    * @param certificatesSize size of the certificates array.
    * @return random ValidatedTransaction object.
    */
-  public static ValidatedTransaction newValidatedTransaction(int certificatesSize) {
+  public static ValidatedTransaction newValidatedTransaction(Identifier sender, Identifier receiver, int certificatesSize) {
     Identifier referenceBlockId = IdentifierFixture.newIdentifier();
-    Identifier sender = IdentifierFixture.newIdentifier();
-    Identifier receiver = IdentifierFixture.newIdentifier();
+
     double amount = 100;
     Signature[] certificates = new Signature[certificatesSize];
     for (int i = 0; i < certificatesSize; i++) {
@@ -98,68 +64,6 @@ public class ValidatedTransactionFixture {
     }
     ValidatedTransaction valTrans = new ValidatedTransaction(referenceBlockId, sender, receiver, amount, certificates);
     Signature sign = SignatureFixture.newSignatureFixture();
-    valTrans.setSignature(sign);
-    return valTrans;
-  }
-
-  /**
-   * Constructor of the validated transactions with randomly generated parameters and given reference block id,
-   * sender and receiver identifier.
-   *
-   * @param referenceBlockId identifier of the reference block.
-   * @param sender           identifier of the sender of this transaction.
-   * @param receiver         identifier of the receiver of this transaction.
-   * @return random ValidatedTransaction object.
-   */
-  public static ValidatedTransaction newValidatedTransaction(Identifier referenceBlockId, Identifier sender,
-                                                             Identifier receiver, Identifier signerId,
-                                                             ArrayList<Account> accounts) {
-    double amount = 100;
-    int certificatesSize = Parameters.SIGNATURE_THRESHOLD;
-    Signature[] certificates = new Signature[certificatesSize];
-    int accountsSize = accounts.size();
-    for (int i = 0; i < certificatesSize; i++) {
-      int signerInd = random.nextInt(accountsSize);
-      while (accounts.get(signerInd).getStake() < Parameters.MINIMUM_STAKE) {
-        signerInd = random.nextInt(accountsSize);
-      }
-      Identifier signer = accounts.get(signerInd).getIdentifier();
-      certificates[i] = SignatureFixture.newSignatureFixture(signer);
-    }
-    ValidatedTransaction valTrans = new ValidatedTransaction(referenceBlockId, sender, receiver, amount, certificates);
-    Signature sign = SignatureFixture.newSignatureFixture(signerId);
-    valTrans.setSignature(sign);
-    return valTrans;
-  }
-
-  /**
-   * Constructor of the validated transactions with randomly generated parameters and given reference block id,
-   * certificates size, sender and receiver identifier.
-   *
-   * @param referenceBlockId identifier of the reference block.
-   * @param sender           identifier of the sender of this transaction.
-   * @param receiver         identifier of the receiver of this transaction.
-   * @param signerId         identifier of the signer of this transaction.
-   * @param certificatesSize size of the certificates array.
-   * @return random ValidatedTransaction object.
-   */
-  public static ValidatedTransaction newValidatedTransaction(Identifier referenceBlockId, Identifier sender,
-                                                             Identifier receiver, Identifier signerId,
-                                                             ArrayList<Account> accounts,
-                                                             int certificatesSize) {
-    double amount = 100;
-    Signature[] certificates = new Signature[certificatesSize];
-    int accountsSize = accounts.size();
-    for (int i = 0; i < certificatesSize; i++) {
-      int signerInd = random.nextInt(accountsSize);
-      while (accounts.get(signerInd).getStake() < Parameters.MINIMUM_STAKE) {
-        signerInd = random.nextInt(accountsSize);
-      }
-      Identifier signer = accounts.get(signerInd).getIdentifier();
-      certificates[i] = SignatureFixture.newSignatureFixture(signer);
-    }
-    ValidatedTransaction valTrans = new ValidatedTransaction(referenceBlockId, sender, receiver, amount, certificates);
-    Signature sign = SignatureFixture.newSignatureFixture(signerId);
     valTrans.setSignature(sign);
     return valTrans;
   }
@@ -182,7 +86,8 @@ public class ValidatedTransactionFixture {
       }
       transactions[i] = ValidatedTransactionFixture.newValidatedTransaction(
           accounts.get(senderIndex).getIdentifier(),
-          accounts.get(receiverIndex).getIdentifier());
+          accounts.get(receiverIndex).getIdentifier(),
+          Parameters.SIGNATURE_THRESHOLD);
     }
 
     return transactions;
