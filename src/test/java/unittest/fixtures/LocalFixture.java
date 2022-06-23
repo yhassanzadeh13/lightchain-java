630c4f25010997d2e42b2889514e4311d8842a72
Yahya <yhassanzadeh13@ku.edu.tr>
2022-06-22 17:51:14 -0700

    adds local fixture

--- src/test/java/unittest/fixtures/LocalFixture.java
+++ src/test/java/unittest/fixtures/LocalFixture.java
@@ -1,2 +1,21 @@
-package unittest.fixtures;public class LocalFixture {
+package unittest.fixtures;
+
+import model.crypto.PrivateKey;
+import model.lightchain.Identifier;
+import model.local.Local;
+
+/**
+ * Encapsulates creating a local for testing.
+ */
+public class LocalFixture {
+  /**
+   * Creates and returns a new local object with random identifier and private key.
+   *
+   * @return new local object with random identifier and private key.
+   */
+  public static Local newLocal() {
+    Identifier localId = IdentifierFixture.newIdentifier();
+    PrivateKey localPrivateKey = KeyGenFixture.newKeyGen().getPrivateKey();
+    return new Local(localId, localPrivateKey);
+  }
 }
