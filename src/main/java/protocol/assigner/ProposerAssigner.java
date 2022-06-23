767c75b1f5879c4abb5eec9ef40ad13948e3e6e9
Yahya <yhassanzadeh13@ku.edu.tr>
2022-06-22 17:51:06 -0700

    adds proposer assigner

---
+++ src/main/java/protocol/assigner/ProposerAssigner.java
@@ -0,0 +1,15 @@
+package protocol.assigner;
+
+import model.lightchain.Identifier;
+import state.Snapshot;
+
+public interface ProposerAssigner {
+  /**
+   * Picks identifier of the proposer of the next block.
+   *
+   * @param currentBlockId identifier of the current block.
+   * @param s snapshot to pick proposer from.
+   * @return identifier of the proposer of the next block.
+   */
+  Identifier nextBlockProposer(Identifier currentBlockId, Snapshot s) throws IllegalArgumentException, IllegalStateException;
+}
---
+++ src/test/java/unittest/fixtures/LocalFixture.java
@@ -0,0 +1,2 @@
+package unittest.fixtures;public class LocalFixture {
+}
