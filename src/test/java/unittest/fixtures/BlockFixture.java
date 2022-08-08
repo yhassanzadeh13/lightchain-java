190225da4bf7cdc779ca967113efba64aede1ef3
Yahya <yhassanzadeh13@ku.edu.tr>
2022-08-08 15:53:21 -0700

    fixes block fixture

--- src/test/java/unittest/fixtures/BlockFixture.java
+++ src/test/java/unittest/fixtures/BlockFixture.java
@@ -3,9 +3,7 @@ package unittest.fixtures;
 import java.util.Random;
 
 import model.crypto.Signature;
-import model.lightchain.Block;
-import model.lightchain.Identifier;
-import model.lightchain.ValidatedTransaction;
+import model.lightchain.*;
 import protocol.Parameters;
 
 /**
@@ -15,56 +13,101 @@ public class BlockFixture {
   private static final Random random = new Random();
 
   /**
+   * Creates a new random block payload with 100 transactions.
+   *
+   * @return a new random block payload with 100 transactions.
+   */
+  public static BlockPayload newBlockPayload() {
+    return new BlockPayload(ValidatedTransactionFixture.newValidatedTransactions(100));
+  }
+
+  /**
+   * Creates a new random block payload.
+   *
+   * @param txCount number of included random transactions.
+   * @return new random block with random transactions.
+   */
+  public static BlockPayload newBlockPayload(int txCount) {
+    return new BlockPayload(ValidatedTransactionFixture.newValidatedTransactions(txCount));
+  }
+
+  /**
+   * Creates a new random block header.
+   *
+   * @return a new random block header.
+   */
+  public static BlockHeader newBlockHeader() {
+    int height = Math.abs(random.nextInt(1_000_000));
+    return new BlockHeader(
+        height,
+        IdentifierFixture.newIdentifier(),
+        IdentifierFixture.newIdentifier(),
+        IdentifierFixture.newIdentifier());
+  }
+
+  /**
+   * Creates a new block proposal with random header and payload.
+   *
+   * @return a new block proposal with random header and payload.
+   */
+  public static BlockProposal newBlockProposal() {
+    return new BlockProposal(newBlockHeader(), newBlockPayload(), SignatureFixture.newSignatureFixture());
+  }
+
+  /**
    * Returns a block with randomly generated values.
    *
    * @return a block with randomly generated values.
    */
   public static Block newBlock() {
-    Identifier previousBlockId = IdentifierFixture.newIdentifier();
-    Identifier proposer = IdentifierFixture.newIdentifier();
-    int validatedTransactionsSize = Parameters.MIN_TRANSACTIONS_NUM + 2;
-    ValidatedTransaction[] transactions = new ValidatedTransaction[validatedTransactionsSize];
-    for (int i = 0; i < validatedTransactionsSize; i++) {
-      transactions[i] = ValidatedTransactionFixture.newValidatedTransaction();
+    int certificatesSize = Parameters.SIGNATURE_THRESHOLD;
+    Signature[] certificates = new Signature[certificatesSize];
+    for (int i = 0; i < certificatesSize; i++) {
+      certificates[i] = SignatureFixture.newSignatureFixture();
     }
-    Signature signature = SignatureFixture.newSignatureFixture(proposer);
 
-    int height = Math.abs(random.nextInt(1_000_000));
-    return new Block(previousBlockId, proposer, height, transactions, signature);
+    return new Block(newBlockProposal(), certificates);
   }
 
   /**
    * Returns a block with randomly generated values and given validated transactions size.
    *
-   * @param validatedTransactionsSize size of the validated transactions array.
+   * @param txCount number of included validated transactions.
    * @return a block with randomly generated values.
    */
-  public static Block newBlock(int validatedTransactionsSize) {
-    Identifier previousBlockId = IdentifierFixture.newIdentifier();
-    Identifier proposer = IdentifierFixture.newIdentifier();
-    ValidatedTransaction[] transactions = new ValidatedTransaction[validatedTransactionsSize];
-    for (int i = 0; i < validatedTransactionsSize; i++) {
-      transactions[i] = ValidatedTransactionFixture.newValidatedTransaction();
+  public static Block newBlock(int txCount) {
+    int certificatesSize = Parameters.SIGNATURE_THRESHOLD;
+    Signature[] certificates = new Signature[certificatesSize];
+    for (int i = 0; i < certificatesSize; i++) {
+      certificates[i] = SignatureFixture.newSignatureFixture();
     }
-    Signature signature = SignatureFixture.newSignatureFixture(proposer);
 
-    int height = Math.abs(random.nextInt(1_000_000));
-    return new Block(previousBlockId, proposer, height, transactions, signature);
+    return new Block(
+        new BlockProposal(
+            newBlockHeader(),
+            newBlockPayload(txCount),
+            SignatureFixture.newSignatureFixture()),
+        certificates);
   }
 
   /**
    * Returns a block with randomly generated values and given validated transactions.
    *
-   * @param transactions validated transactions array.
+   * @param transactions validated transactions to be included in the block. 
    * @return a block with randomly generated values.
    */
   public static Block newBlock(ValidatedTransaction[] transactions) {
-    Identifier previousBlockId = IdentifierFixture.newIdentifier();
-    Identifier proposer = IdentifierFixture.newIdentifier();
-    Signature signature = SignatureFixture.newSignatureFixture(proposer);
-
-    int height = Math.abs(random.nextInt(1_000_000));
-    return new Block(previousBlockId, proposer, height, transactions, signature);
+    int certificatesSize = Parameters.SIGNATURE_THRESHOLD;
+    Signature[] certificates = new Signature[certificatesSize];
+    for (int i = 0; i < certificatesSize; i++) {
+      certificates[i] = SignatureFixture.newSignatureFixture();
+    }
+    return new Block(
+        new BlockProposal(
+            newBlockHeader(),
+            new BlockPayload(transactions),
+            SignatureFixture.newSignatureFixture()),
+        certificates);
   }
 
 }
