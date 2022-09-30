2508835b6806285f460a795b02e8f50e8035df7c
Yahya <yhassanzadeh13@ku.edu.tr>
2022-09-30 06:10:22 -0700

    extends log levels

--- src/main/java/modules/logger/Logger.java
+++ src/main/java/modules/logger/Logger.java
@@ -40,15 +40,55 @@ public class Logger {
     this.logger.warn(message);
   }
 
+  public void warn(String message, Throwable t) {
+    this.logger.warn(message, t);
+  }
+
+  public void warn(String message, Object... args) {
+    this.logger.warn(message, args);
+  }
+
+  public void warn(String message, Throwable t, Object... args) {
+    this.logger.warn(message, t, args);
+  }
+
   public void info(String message) {
     this.logger.info(message);
+  }
+
+  public void info(String message, Object... args) {
+    this.logger.info(message, args);
   }
 
   public void debug(String message) {
     this.logger.debug(message);
   }
 
+  public void debug(String message, Object... args) {
+    this.logger.debug(message, args);
+  }
+
   public void trace(String message) {
     this.logger.trace(message);
+  }
+
+  public void trace(String message, Object... args) {
+    this.logger.trace(message, args);
+  }
+
+  public void error(String message) {
+    this.logger.error(message);
+  }
+
+  public void error(String message, Throwable t) {
+    this.logger.error(message, t);
+  }
+
+  public void error(String message, Object... args) {
+    this.logger.error(message, args);
+  }
+
+  public void error(String message, Throwable t, Object... args) {
+    this.logger.error(message, t, args);
   }
 }
