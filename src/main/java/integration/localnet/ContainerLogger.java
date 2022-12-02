0fe8822375d372495622c93e153d4517de6586db
Yahya <yhassanzadeh13@ku.edu.tr>
2022-12-02 07:04:28 -0800

    temp wip

--- src/main/java/integration/localnet/ContainerLogger.java
+++ src/main/java/integration/localnet/ContainerLogger.java
@@ -17,11 +17,19 @@ public class ContainerLogger {
   private static final int DEFAULT_LAST_LOG_TIME = 0;
   DockerClient dockerClient;
   private final ConcurrentHashMap<String, Integer> lastLogTime;
+
+  private final Hashtable<String, String> loggingBuffer;
+
+  /**
+   * Minimum length of a log to be printed.
+   */
+  private static final int MIN_LOG_LENGTH = 30;
   private final Logger logger = LightchainLogger.getLogger(ContainerLogger.class.getCanonicalName());
 
   public ContainerLogger(DockerClient dockerClient) {
     this.dockerClient = dockerClient;
     this.lastLogTime = new ConcurrentHashMap<>();
+    this.loggingBuffer = new Hashtable<>();
   }
 
   public void registerLogger(String containerId) {
@@ -47,14 +55,79 @@ public class ContainerLogger {
           @Override
           public void onNext(Frame item) {
             super.onNext(item);
-            String log = new String(item.getPayload(), StandardCharsets.UTF_8);
-            logger.info("node_id {}: {}", c.getKey(), log);
+            String lg = new String(item.getPayload(), StandardCharsets.UTF_8);
+            String[] split = lg.split("\n");
+            System.out.println("-------------------");
+            for(String s : split) {
+                System.out.println("[Container] " + s);
+            }
+            System.out.println("-------------------");
+            // inspectLog(c.getKey(), lg);
           }
-        }).awaitCompletion(1, TimeUnit.SECONDS);
+        }).awaitCompletion();
       } catch (InterruptedException e) {
         logger.fatal("error while logging container: {}", c.getKey(), e);
       }
       this.lastLogTime.put(c.getKey(), (int) (System.currentTimeMillis() / 1000));
+    }
+  }
+
+  LogContainerResultCallback loggingCallback = new
+      LogContainerResultCallback();
+
+  /**
+   * Buffers the log message from the container.
+   *
+   * @param containerId the container id.
+   * @param log the log message.
+   */
+  private void bufferLog(String containerId, String log) {
+    if (this.loggingBuffer.containsKey(containerId)) {
+      this.loggingBuffer.put(containerId, this.loggingBuffer.get(containerId) + log);
+    } else {
+      this.loggingBuffer.put(containerId, log);
+    }
+  }
+
+  /**
+   * Flushes the log message from the container.
+   *
+   * @param containerId the container id.
+   */
+  private void flushLog(String containerId) {
+    if (this.loggingBuffer.containsKey(containerId)) {
+      this.logger.info("node_id {}: {}", containerId, this.loggingBuffer.get(containerId));
+      this.loggingBuffer.remove(containerId);
+    }
+  }
+
+  /**
+   * Logs the log message from the container.
+   *
+   * @param containerId the container id.
+   * @param log the log message.
+   */
+  private void inspectLog(String containerId, String log) {
+    log = log.trim();
+    if (log.length() < MIN_LOG_LENGTH) {
+      bufferAndFlushLog(containerId, log);
+    } else {
+      flushLog(containerId);
+      this.logger.info("node_id {}: {}", containerId, log);
+    }
+  }
+
+  /**
+   * Buffers the log message from the container and flushes if the buffered log message is
+   * longer than the minimum length.
+   *
+   * @param containerId the container id.
+   * @param log the log message.
+   */
+  private void bufferAndFlushLog(String containerId, String log) {
+    bufferLog(containerId, log);
+    if(this.loggingBuffer.get(containerId).length() > MIN_LOG_LENGTH) {
+      flushLog(containerId);
     }
   }
 }
